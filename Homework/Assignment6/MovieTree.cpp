45e36fb0aa332b8fb46a5bfb3f83842cfdf5dfb7
Zach(PC) <griffith.zach13@gmail.com>
2019-10-18 10:57:21 -0600

    Driver created, readfile implemented

--- Homework/Assignment6/MovieTree.cpp
+++ Homework/Assignment6/MovieTree.cpp
@@ -10,22 +10,52 @@ using namespace std;
 
 MovieTree::MovieTree() {
   //write your code
+	ranking = 0;
+	title = "";
+	year = 0;
+	rating = 0.0;
+
 }
 
 MovieTree::~MovieTree() {
-  //write your code
+
 }
 
 void MovieTree::printMovieInventory() {
-   //write your code
+
+	if(root!=NULL)
+	{
+		MovieNode *crl = root;
+		cout ​ << ​ "Movie: "​ << m->title << ​ " "​ << m->rating << ​ endl​;
+	}
+	else
+		cout ​ << ​ "Tree is Empty. Cannot print"​ << ​ endl​;
+	
 }
 
 void MovieTree::addMovieNode(int ranking, string title, int year, float rating) {
-  //write your code
+  
+  MovieNode *m = MovieNode(ranking, title, year, rating);
+
+  if(root==NULL) {
+  	root = m;
+  	m->right = NULL;
+  	m->left = NULL;
+  	return
+  }
+  
+  if(m->title[0] < root->title[0]) {
+  	m->left =
+  }
+
+
+
+
 }
 
 void MovieTree::findMovie(string title) {
-  //write your code
+  
+  // Search tree till matching title is found
 }
 
 void MovieTree::queryMovies(float rating, int year) {
---
+++ Homework/Assignment6/driver.cpp
@@ -0,0 +1,93 @@
+#include "MovieTree.hpp"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int split (string str, char c, string fill[])
+{
+    if (str.length() == 0) {
+        return 0;
+    }
+    
+    string word = "";
+    int counter = 0;
+    str = str + c;
+    
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == c)
+        {
+        	if (word.length() == 0)
+        	{
+        	    continue;
+        	}
+        	
+        	fill[counter] = word;
+            counter++;
+            word = "";
+        } 
+        else 
+        {
+            word = word + str[i];
+        }
+    }
+    return counter;
+}
+
+int readFile(string fileName, MovieTree m) {
+
+	ifstream data;
+	data.open(movieFile);
+
+	string temp[4];
+	int rank;
+	string title;
+	int year;
+	float rating;
+
+	string line;
+	if(data.is_open()) {
+
+		while(getline(data, line)) {
+
+			split(line, ",", temp);
+			rank = stoi(temp[0]);
+			title = temp[1];
+			year = stoi(temp[2]);
+			rating = stof(temp[3]);
+			m.addMovieNode(rank, title, year, rating);
+		}
+		data.close();
+		return 1;
+	}
+	else
+		return 0;
+}
+
+void displayMenu() {
+
+	cout​ << ​ "======Main Menu======"​ << ​ endl​;
+	cout​ << "1. Find a movie"​ << ​ endl​;
+	cout​ << "2. Query movies"​ << ​ endl​;
+	cout​ << "3. Print the inventory"​ << ​ endl​;
+	cout​ << "4. Average Rating of movies"​ << ​ endl​;
+	cout​ << "5. Find a movie"​ << ​ endl​;
+
+}
+ 
+int main(int argc, char const *argv[]) {
+	
+	MovieTree m;
+	string movieFile = argv[1];
+	string docFile = argv[2];
+
+	readFile(movieFile, m);
+}
+
+
+
+
+
+
+
