2d9e0e8e5dd2c95f0fac18ff31fc0b5a4c5bf4dd
Zach(PC) <griffith.zach13@gmail.com>
2019-10-25 11:21:01 -0600

    notes

--- Other/quickTest.cpp
+++ Other/quickTest.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int findSum(TreeNode * root, int depth)
+{
+    if(root != NULL)
+    {
+        int rightSum = findSum(root->right, depth + 1);
+        int leftSum = findSum(root->left, depth + 1);
+        cout << root->key;
+        if (depth != 0)
+        {
+            cout << " ";
+        }
+        return rightSum + leftSum + root->key;
+    }
+    else
+       return 0;
+}
+
 int main()
 {
 	cout << "Hello World" << endl;
